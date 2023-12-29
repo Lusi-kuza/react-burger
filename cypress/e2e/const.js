288bbc8f449bcc2f2807008e5f3a4923a920e935
EkaterinaZ <kuza.fex@yandex.ru>
2023-12-29 14:14:12 +0700

    Updates

--- cypress/e2e/const.js
+++
@@ -1,5 +0,0 @@
-export const bunCategory = '[data-testid="Булки"]';
-export const souseCategory = '[data-testid="Соусы"]';
-export const bunDropTarget = '[data-testid="bunDropTarget"]';
-export const fillingDropTarget = '[data-testid="fillingDropTarget"]';
-export const titleBurgerIngredients = '[data-testid="titleBurgerIngredients"]';
--- src/services/appContext.js
+++
@@ -1,4 +0,0 @@
-import React, { createContext } from "react";
-
-export const BurgerContext = createContext();
-export const ConstructorIngredientsContext = createContext();
